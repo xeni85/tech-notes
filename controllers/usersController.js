d644baf5e3166805a98266ac00c22e71919e20c7
xeni85 <shkelzen.dunisha@umsh.edu.al>
2023-04-14 22:20:48 -0400

    creating update function

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -19,7 +19,32 @@ const getAllUsers = asyncHandler(async (req, res, next) => {
 //@access Private
 
 const createNewUser = asyncHandler(async (req, res, next) => {
-    
+    const { username, password, roles } = req.body;
+
+    //Confirm data
+    if(!username || !password || !Array.isArray(roles) || !roles.length) {
+        return res.status(400).json({message: 'All fields are required'}); 
+    }
+
+    //Check for duplicates
+    const duplicate = await User.findOne({username}).lean().exec();
+    if(duplicate) {
+        return res.status(409).json({message: 'Duplicate username'})
+    }
+
+    //hash the password
+    const hashedPwd = await bcrypt.hash(password, 10) //salt rounds
+
+    const userObject = { username, "password": hashedPwd, roles }
+
+    //Create and store new user
+    const user = await User.create(userObject)
+
+    if (user) { //created
+        res.status(201).json({message: `New user ${username} created`})
+    } else { //
+        res.status(400).json({message: 'Invalid user data received'});
+    }
 })
 
 //@desc Update a user
@@ -27,7 +52,12 @@ const createNewUser = asyncHandler(async (req, res, next) => {
 //@access Private
 
 const updateUser = asyncHandler(async (req, res, next) => {
+    const { id, username, roles, active, password } = req.body
 
+    //confirm data
+    if(!id || !username || !Array.isArray(roles) || !roles.length || typeof active != boolean) {
+        
+    }
 })
 
 //@desc Delete a user
