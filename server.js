a02476a0dd790b70ea4a3f240fc65d20a94ff1d3
xeni85 <shkelzen.dunisha@umsh.edu.al>
2023-04-14 21:47:34 -0400

    connected to mongo DB, working on the backend

--- server.js
+++ server.js
@@ -2,23 +2,33 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
-const PORT = process.env.PORT || 3000;
 const { logger } = require('./middleware/logger');
 const errorHandler = require('./middleware/errorHandler');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const corsOptions = require('./config/corsOptions');
+const connectDB = require('./config/dbConn')
+const mongoose = require('mongoose');
+const {logEvents} = require('./middleware/logger')
+const PORT = process.env.PORT || 3000;
+
 
 console.log(process.env.NODE_ENV)
 
+connectDB()
+
 app.use(logger);
-//add json data
+
 app.use(cors(corsOptions));
+
+//add json data
 app.use(express.json());
+
 //long version of app.use(express.static('public'));
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.use(cookieParser());
+
 app.use('/', require('./routes/root'))
 
 app.all('*', (req, res) => {
@@ -33,5 +43,13 @@ app.all('*', (req, res) => {
 })
 
 app.use(errorHandler);
-app.listen(PORT, () => console.log('listening on port ' + PORT));
-    
+
+mongoose.connection.once('open', ()=> {
+    console.log('connected to mongoDB');
+    app.listen(PORT, () => console.log('listening on port ' + PORT));
+})
+
+mongoose.connection.on('error', err => {
+    console.log(err)
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log');
+})
